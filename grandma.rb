28f763f6016851a7bdbbb8ad0eb4c30e958a969a
Zachary Schulz <zjschulz@gmail.com>
2019-09-11 00:33:55 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,5 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(greeting)
-  if greeting == greeting.upcase
-    puts "NO, NOT SINCE 1938!"
-  else
     puts "HUH?! SPEAK UP, SONNY!"
   end
 end
