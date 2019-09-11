d07e19ad8ba0ccd6b5e2f6f4fa1919a9896ef93d
Zachary Schulz <zjschulz@gmail.com>
2019-09-11 00:50:07 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,7 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(words)
-  puts "BOO"
+  if #(words) = #(words).upcase
+  puts
 end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
