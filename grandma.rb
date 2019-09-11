3496d83b3844158ca40352f68d62331ca99e39a8
Zachary Schulz <zjschulz@gmail.com>
2019-09-11 00:32:29 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,9 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(greeting)
-  
+  if greeting 
+else
+  puts 
+end
 end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
