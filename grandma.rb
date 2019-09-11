f450b7fc323c55e06584fd34008a953f625a9b64
Zachary Schulz <zjschulz@gmail.com>
2019-09-11 00:41:27 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,7 @@
 # Write a speak_to_grandma method.
-
+def speak_to_grandma
+  puts "BOO"
+end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
